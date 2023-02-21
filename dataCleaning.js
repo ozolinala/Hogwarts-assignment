808f8ddd5f7e343eebc92b273dcb3ec2f457fca0
Laura Ozolina <laur755v@stud.kea.dk>
2023-02-21 15:43:04 +0100

    clean data showing

--- dataCleaning.js
+++ dataCleaning.js
@@ -36,12 +36,69 @@ const Student = {
 
 //   fetch the data, call the prepareObjects() function for each student in the data using
  function start(){
-
+    fetch(url)
+    .then((response) => response.json())
+    .then((allStudentData) => allStudentData.forEach(prepareObjects));
   }
 
 //   trimm whitespace, capitalize names and nicknames, create a new Student object, and then push the new object into the allStudents array 
-  function prepareObjects(){
 
+function prepareObjects(studentObject) {
+    let fullname = studentObject.fullname.trim();
+    let newFullname = capitalization(fullname);
+    const student = Object.create(Student);
+    let middle = newFullname.substring(newFullname.indexOf(" ") + 1, newFullname.lastIndexOf(" "));
+    student.firstName = newFullname.substring(0, newFullname.indexOf(" ") + 1);
+    student.lastName = newFullname.substring(newFullname.lastIndexOf(" ") + 1);
+    if (middle.charAt(0) === '"' && middle.charAt(middle.length - 1) === '"') {
+      middle = middle.substring(1, middle.length - 1); // remove the quotes
+      student.nickName = middle;
+    } else {
+      student.middleName = middle;
+    }
+    student.image = `images/${newFullname.toLowerCase().replace(/\s/g, '-')}.png`;
+    let newHouse = capitalization(studentObject.house.trim());
+    student.house = newHouse;
+    allStudents.push(student);
+    displayList();
+  }
+
+  function capitalization(fullname) {
+    fullname = fullname.toLowerCase();
+    let fullnameArray = fullname.split("");
+    fullnameArray[0] = fullnameArray[0].toUpperCase();
+    for (let i = 0; i <= fullnameArray.length; i++) {
+      if (fullnameArray[i] === " " || fullnameArray[i] === "-" || fullnameArray[i] === '"') {
+        fullnameArray[i + 1] = fullnameArray[i + 1].charAt(0).toUpperCase();
+      }
+    }
+    console.log("Capitalization log", fullnameArray);
+    fullname = fullnameArray.join("");
+    return fullname;
+  }
+  
+  function displayList() {
+    // clear the list
+    document.querySelector("#list tbody").innerHTML = "";
+  
+    // build a new list
+    allStudents.forEach(displayStudent);
+  }
+  
+  function displayStudent(alumni) {
+    // create clone
+    const clone = document.querySelector("template#alumni").content.cloneNode(true);
+  
+    // set clone data
+    clone.querySelector("[data-field=firstName]").textContent = alumni.firstName;
+    clone.querySelector("[data-field=lastName]").textContent = alumni.lastName;
+    clone.querySelector("[data-field=middleName]").textContent = alumni.middleName;
+    clone.querySelector("[data-field=nickName]").textContent = alumni.nickName;
+    clone.querySelector("[data-field=image]").textContent = alumni.image;
+    clone.querySelector("[data-field=house]").textContent = alumni.house;
+  
+    // append clone to list
+    document.querySelector("#list tbody").appendChild(clone);
   }
 
 
