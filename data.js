3bd3514ddad94fc341f5611b090f96aeb90b567b
Laura Ozolina <laur755v@stud.kea.dk>
2023-04-04 17:50:12 +0200

    restarting the js file

--- data.js
+++ data.js
@@ -11,47 +11,79 @@ let halfB = [];
 let pureB = [];
 let allStudents = [];
 let filteredArray;
+// let filteredList;
+// let buttonDataFilter = {filter: "*"};
 
 
 // an object that serves as a template for each student object
 const Student = {
-    
     firstName: "",
     lastName: "",
     middleName: "",
     nickName: "",
+    gender:"",
     image: "", 
     house: "",
     blood:"",
+    prefect:false,
+    inquisitor:false,
+    expelled:false,
   };
+const settings = {
+  filter:"enrolled",
+  sortBy:"firstname",
+  sortDir:"asc",
 
-//   fetch the data, call the prepareObjects() function for each student in the data 
+}
+
+//   fetch the data, add the event listeners, call the prepareObjects() function for each student in the data 
 function start() {
   console.log("lets begin");
-  loadJson1();
+  loadJSON();
+  registerButtons();
+  console.log(allStudents);
+}
+function registerButtons() {
+  document.querySelectorAll("[data-action='filter']").forEach((button) => button.addEventListener("click", selectFilter));
+  document.querySelectorAll("[data-action='sort']").forEach((button) => button.addEventListener("click", selectSort));
+  document.querySelector("#search-bar").addEventListener("input", searching);
+  //Hack the system
+//   document.querySelector("#enterthevoid").addEventListener("click", hackSystem);
+// 
 }
 
-async function loadJson1() {
-  const resp = await fetch(familiesUrl);
-  const data = await resp.json();
-  console.log("Blood data loaded");
-  halfB = data.half;
-  pureB = data.pure;
-
-  loadJson2();
+async function loadJSON() {
+  await Promise.all([fetch("https://petlatkea.dk/2021/hogwarts/students.json").then((res) => res.json()), fetch("https://petlatkea.dk/2021/hogwarts/families.json").then((res) => res.json())]).then((jsonData) => {
+    // When loaded, prepare data objects
+    prepareObjects(jsonData[0], jsonData[1]);
+  });
 }
+// async function loadJson1() {
+//   const resp = await fetch(familiesUrl);
+//   const data = await resp.json();
+//   console.log("Blood data loaded");
+//   halfB = data.half;
+//   pureB = data.pure;
+//   loadJson2();
+// }
 
-async function loadJson2() {
-  const resp = await fetch(url);
-  const data = await resp.json();
-  console.log("Student data loaded");
-  prepareObjects(data);
-}
+// async function loadJson2() {
+//   const resp = await fetch(url);
+//   const data = await resp.json();
+//   console.log("Student data loaded");
+//   prepareObjects(data);
+// }
 function prepareObjects(jsonData) {
   allStudents = jsonData.map(prepareObject);
   filteredArray = allStudents;
+  buildList();
 }
 
+function buildList(){
+  const currentList = filteredArray(allStudents);
+  displayList(filteredArray);
+  console.log("hi")
+}
 
 //   trimm whitespace, capitalize names and nicknames, create a new Student object, and then push the new object into the allStudents array 
 
@@ -71,8 +103,8 @@ function prepareObject(studentObject) {
     student.image = getPortrait(newFullname);    
     let newHouse = capitalization(studentObject.house.trim());
     student.house = newHouse;
-    allStudents.push(student);
-    displayList();
+   console.log(allStudents.push(student));
+    displayList(allStudents);
   }
 
   function capitalization(fullname) {
@@ -98,12 +130,14 @@ function prepareObject(studentObject) {
     if (firstName === "") {
       urlImage = "empty.png";
     }
+    // CODE FOR PATIL SISTERS
     if (lastName === "patil") {
       console.log("Searching for Patil sisters");
       urlImage = lastName + "_" + firstName + ".png";
     }
-  
+  // CODE FOR JUSTIN
     if (lastName.includes("-")) {
+      console.log("Searching for Justin");
       lastName = lastName.substring(lastName.indexOf("-") + 1);
       urlImage = lastName + "_" + firstName.charAt(0) + ".png";
       return urlImage;
@@ -111,9 +145,10 @@ function prepareObject(studentObject) {
       return urlImage;
     }
   }
-
-
-  function displayList() {
+// ----------------------------------------
+  function displayList(list) {
+    allStudents = list;
+    console.log(allStudents);
     // clear the list
     document.querySelector("#list tbody").innerHTML = "";
   
@@ -126,8 +161,9 @@ function prepareObject(studentObject) {
     const clone = document.querySelector("template#wizards").content.cloneNode(true);
   
     // set clone data
-
+console.log(clone);
     clone.querySelector("[data-field=firstName]").textContent = wizards.firstName;
+    console.log(wizards.firstName);
     clone.querySelector("[data-field=lastName]").textContent = wizards.lastName;
     clone.querySelector("[data-field=middleName]").textContent = wizards.middleName;
     clone.querySelector("[data-field=nickName]").textContent = wizards.nickName;
@@ -137,7 +173,53 @@ function prepareObject(studentObject) {
     document.querySelector("#list tbody").appendChild(clone);
   }
 
-// TODO: FILTERING
+//  ------------------- FILTERING ---------------------
+
+// tried to filter like the animal base
+// function selectFilter(event){
+//   let filteredList = all;
+//   buttonDataFilter = event.target.dataset.filter;
+//   if (buttonDataFilter !== "*") {
+//       filteredList = allStudents.filter(whichHouse);
+//   } else {
+//       filteredList = allStudents;
+//   }
+//   displayList(filteredList);
+// }
+// function whichHouse(wizards){
+//   if (wizards.house === buttonDataFilter){
+//       return true;
+//   }
+// }
+// FILTERING
+// const filterButtons = document.querySelectorAll('.filter');
+
+// filterButtons.forEach(button => {
+//   button.addEventListener('click', (event) => {
+//     const filter = event.target.dataset.filter;
+//     const studentList = document.querySelectorAll('#list tbody tr');
+
+//     studentList.forEach((s) => {
+//       if (filter === '*') {
+//         // console.log(s);
+//         s.style.display = '';
+//       console.log("this is the filter", `${filter}`)
+//         return;
+//       }
+
+//       if (s.house === filter )
+//         // || student.dataset.enrolled === filter || student.dataset.expelled === filter) 
+//         {
+//           console.log("this is the filter", `${filter}`, `${s.house}`)
+//         s.style.display = '';
+//       } else {
+//         s.style.display = 'none';
+//       }
+//     });
+//   });
+// });
+
 // TODO: SORTING
 // TODO: INQUISITIONAL LIST
+// TODO: EXPELLING
 // TODO: HACKING
